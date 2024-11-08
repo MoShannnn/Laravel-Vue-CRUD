fc57127744ab7155c7528087d43638b7bcb841ab
Mo Shan <moshan2500@gmail.com>
2024-11-09 00:04:45 +0630

    adde frontend vue

--- lv-backend/database/migrations/2024_11_08_160453_create_employees_table.php
+++ lv-backend/database/migrations/2024_11_08_160453_create_employees_table.php
@@ -7,7 +7,7 @@ return new class extends Migration
   
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('address');
